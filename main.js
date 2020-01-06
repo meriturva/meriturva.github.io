c6116052859d3e4e89db4edbf2e6b9d2b84eb02a
Diego Bonura <diego@bonura.dev>
2020-01-06 14:37:15 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready( function() {	
+  function fullhome() {
+      var hometext = $('.home')            
+      hometext.css({
+          "height": $(window).height() + "px"
+      });
+  }
+  fullhome();
+  $(window).resize(fullhome);
+    
+  var typed = new Typed('.element', {
+    strings: ["projects have to go.", "projects could go wrong.", "projects can be fixed.", "projects could be useless.", "projects bring value.", "projects could only be costs."], 
+    loop: true ,
+    backDelay: 1500,
+    backSpeed: 60,                
+    typeSpeed: 60,
+  });
+  
+  particlesJS.load('particles', 'particles.json', function() {
+      jQuery(".loader").fadeOut("slow");
+      jQuery(".preloader").delay(1000).fadeOut("slow");
+
+  });   
+});
